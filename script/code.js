868be336d59067d99c11a693e2d7069aa2f8aa27
LeahBasson <leahbasson54@gmail.com>
2024-05-14 13:58:16 +0200

    Exercise Completed

---
+++ script/code.js
@@ -0,0 +1,9 @@
+let firstName = "Leah", lastName = "Basson", numberVar = 5 
+ 
+console.log(` ${firstName} ${lastName} ${numberVar*5}`)
+// The output is 25
+
+const firstName = "Leah"
+let firstName = "Surname"
+console.log(`${firstName}`)
+// I got an error
